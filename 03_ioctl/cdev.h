cadc00fdfcc11a89dcfdfb6c629e20c03cba1cf6
Brett Holman <bpholman5@gmail.com>
2021-01-10 00:56:14 -0500

    from /usr/include/asm/ioctl.h - where the IOR and IOW macros are
     * NOTE: _IOW means userland is writing and kernel is reading. _IOR
     * means userland is reading and kernel is writing.

--- 03_ioctl/cdev.h
+++ 03_ioctl/cdev.h
@@ -6,6 +6,6 @@
 
 // IOCTL calls
 #define IOCTL_MAGIC 0x70
-#define IOCTL_SET_MSG 		_IOR(IOCTL_MAGIC, 0, char *) 
+#define IOCTL_SET_MSG 		_IOW(IOCTL_MAGIC, 0, char *) 
 #define IOCTL_GET_MSG 		_IOR(IOCTL_MAGIC, 1, char *) 
 #define IOCTL_GET_NTH_BYTE 	_IOR(IOCTL_MAGIC, 2, int) 
